bc288bfe9f33d1bfbbf461e606ea7e6d1b3403fd
Nameless <keirhavel@live.com>
2024-12-30 08:25:00 -0800

    hippo bug fix and adding C version

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Forecasting
-Repository containing time-series analysis methods, in particular neural Koopman operators.
+Repository containing time-series analysis methods, such as HiPPO and neural Koopman operators.
 
 1. The lkis.py file contains an implementation of [Learning Koopman Invariant Subspaces for Dynamic Mode Decomposition](https://arxiv.org/pdf/1710.04340.pdf), a neural network approach for learning the measurement operators in Koopman theory / Dynamic Mode Decomposition.
 2. The koopman_neural_forcaster.py file contains an implementation of [Koopman Neural Forecaster for Time Series with Temporal Distribution Shifts](https://arxiv.org/pdf/2210.03675).
+3. The hippo.py file contains an implementation of [HiPPO-LegS](https://arxiv.org/abs/2008.07669). Note that the official version has better speed and stability, so you should [compile the official C++ kernel from here](https://github.com/HazyResearch/hippo-code)
---
+++ khresmoi/hippo_forecaster.py
@@ -0,0 +1,93 @@
+import torch
+from torch.special import legendre_polynomial_p
+try:
+    import hippo
+    HIPPO_AVAILABLE = True
+except ImportError:
+    from warnings import warn
+    warn("hippo not found, using slow version")
+    HIPPO_AVAILABLE = False
+
+class HiPPOLegS(torch.nn.Module):
+    def __init__(self, coef_dim, use_cpp = True):
+        super().__init__()
+        self.coef_dim = coef_dim
+        self.use_cpp = use_cpp
+        self.curr_t = None
+        A, B = self._initialize_weights(coef_dim)
+        self.register_buffer("A", A)
+        self.register_buffer("B", B)
+        self.register_buffer("coef", torch.zeros(coef_dim))
+
+    def _initialize_weights(self, coef_dim):
+        A = torch.diag(torch.arange(1,
+                                    coef_dim + 1,
+                                    dtype=torch.float32))
+        
+        B = torch.arange(0, coef_dim, dtype=torch.float32)
+        B = torch.sqrt(2*B + 1)
+
+        outer_product = torch.einsum("i,j->ij", B, B)
+        A += outer_product.tril(diagonal=-1)
+        return A, B
+    
+    def forward(self, t):
+        """
+        Reconstuct the signal at times t.
+        """
+        out = torch.zeros_like(t)
+        t_scaled = (2 * t / self.curr_t) - 1
+        #TODO : can we vectorize this?
+        for i in range(self.coef_dim):
+            sqrt_ = torch.sqrt(torch.tensor(2 * i + 1))
+            lp = legendre_polynomial_p(t_scaled, i)
+
+            out += self.coef[i] * sqrt_* lp
+        return out
+
+    def fit_coef(self, f_t, t_space):
+        if HIPPO_AVAILABLE and self.use_cpp:
+            c_k = hippo.legs_function_approx_trapezoidal(f_t, self.coef_dim)
+            t = t_space[-1]
+        else:
+            c_k = self.coef.clone()
+            for k in range(f_t.shape[0]):
+                t = t_space[k]
+                k_inv = 1 / (k + 1)
+                b_term = k_inv * self.B * f_t[k]
+                c_k = c_k - (k_inv * self.A @ c_k) + b_term
+                #print(c_k.norm())
+
+        self.coef = c_k
+        self.curr_t = t
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    # note that the slow version collapses for coef_dim >~ 32
+    coef_dim = 32
+
+    hippo_model = HiPPOLegS(coef_dim , use_cpp = True)
+    hippo_model_slow = HiPPOLegS(coef_dim , use_cpp = False)
+
+    t_space = torch.linspace(0, 1, 1000)
+    simple_signal = torch.zeros_like(t_space)
+    for i in range(10):
+        simple_signal += torch.randn(1) * torch.sin(i * torch.pi * t_space)
+    rand_walk = (torch.randn_like(t_space) * 1).cumsum(-1)
+    simple_signal += rand_walk
+
+    signal_np = simple_signal.cpu().numpy()
+    plt.plot(t_space, signal_np, label = "Signal")
+
+    hippo_model.fit_coef(simple_signal, t_space)
+    hippo_model_slow.fit_coef(simple_signal, t_space)
+
+    approx = hippo_model(t_space)
+    approx_np = approx.cpu().numpy()
+    plt.plot(t_space, approx_np, label = "HiPPOLegS")
+
+    approx_slow = hippo_model_slow(t_space)
+    approx_slow_np = approx_slow.cpu().numpy()
+    plt.plot(t_space, approx_slow_np, label = "HiPPOLegS Slow")
+    plt.legend()
+    
